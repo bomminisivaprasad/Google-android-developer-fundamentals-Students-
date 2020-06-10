ae8f2344ac3760c326b612ac97f0d4f410369626
unknown <g.v.saisankar@gmail.com>
2020-06-10 21:56:05 +0530

    TabView Updated

---
+++ TabView/app/src/main/java/com/example/tabview/Status.java
@@ -0,0 +1,27 @@
+package com.example.tabview;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Status extends Fragment {
+
+    public Status() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v=inflater.inflate(R.layout.fragment_status, container, false);
+
+        return v;
+    }
+}
